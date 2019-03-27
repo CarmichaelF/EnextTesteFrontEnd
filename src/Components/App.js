2ff98989ed39f709873831a5d5d18da22a85bb2e
Carmichael Ferreira Pereira <carmichaelferreira06@gmail.com>
2019-03-27 17:10:11 -0300

    add: New components structure was added.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./Header";
+import Potions from "./Potions";
+import Footer from "./Footer";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <>
+        <Header />
+        <Potions />
+        <Footer />
+      </>
     );
   }
 }
---
+++ src/Components/Potions.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Potions.css";
+
+class Potions extends Component {
+  render() {
+    return <div className="Header">Potions</div>;
+  }
+}
+
+export default Potions;
