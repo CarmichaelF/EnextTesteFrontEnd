c6be735348108e77b1e7f582830ec6b2be1f2b84
Carmichael Ferreira Pereira <carmichaelferreira06@gmail.com>
2019-03-28 15:08:09 -0300

    add: API call was added.

--- src/Components/Potions-Section/Potion.js
+++ src/Components/Potions-Section/Potion.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import "./Potion.css";
 
+
 class Potion extends Component {
+
   render() {
     return <div className="Header">Potion</div>;
   }
--- src/Components/Potions-Section/Potions.js
+++ src/Components/Potions-Section/Potions.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
+import Potion from "./Potion";
+import getAll from "../../Utils/PotionsAPI";
 import "./Potions.css";
 
 class Potions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { potions: null };
+  }
+
+  componentDidMount() {
+    getAll().then(potions => this.setState({ potions }))
+    .catch(error => console.error(error));
+  }
+
   render() {
-    return <div className="Header">Potions</div>;
+    return (
+      <div className="potions">
+        <h1 className= "potions-title">Potions:</h1>
+        
+      </div>
+    );
   }
 }
 
