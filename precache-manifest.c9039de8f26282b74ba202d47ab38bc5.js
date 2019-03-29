4476a2dbeeafa665d518c680ca286ea280b5fc35
Carmichael Ferreira Pereira <carmichaelferreira06@gmail.com>
2019-03-29 17:14:10 -0300

    Updates

---
+++ precache-manifest.c9039de8f26282b74ba202d47ab38bc5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "67feeedcb068e5ab1fcdb08b137c20f6",
+    "url": "/EnextTesteFrontEnd/static/media/dragon-tonic.67feeedc.png"
+  },
+  {
+    "revision": "5087f6b395815977297c",
+    "url": "/EnextTesteFrontEnd/static/css/main.7209df92.chunk.css"
+  },
+  {
+    "revision": "4bd272f325bbd69eab22",
+    "url": "/EnextTesteFrontEnd/static/js/runtime~main.e565dba0.js"
+  },
+  {
+    "revision": "a7cd20020712ac99b275",
+    "url": "/EnextTesteFrontEnd/static/js/2.65917f1d.chunk.js"
+  },
+  {
+    "revision": "7287aa56ef46298c121ceae4d3fece1f",
+    "url": "/EnextTesteFrontEnd/static/media/aging-potion.7287aa56.png"
+  },
+  {
+    "revision": "4bd51703afdf18d59908f9fe9f0cedd8",
+    "url": "/EnextTesteFrontEnd/static/media/bulgeye-potion.4bd51703.png"
+  },
+  {
+    "revision": "5087f6b395815977297c",
+    "url": "/EnextTesteFrontEnd/static/js/main.93f96576.chunk.js"
+  },
+  {
+    "revision": "6521c7065b8bda81028ce9ad7b7056f2",
+    "url": "/EnextTesteFrontEnd/static/media/love-potion.6521c706.png"
+  },
+  {
+    "revision": "23d73ce737fe46924e466b878454c077",
+    "url": "/EnextTesteFrontEnd/static/media/polyjuice-potion.23d73ce7.png"
+  },
+  {
+    "revision": "ab39e5cc31120f07640e101e309d9ec0",
+    "url": "/EnextTesteFrontEnd/static/media/sleeping-draught.ab39e5cc.png"
+  },
+  {
+    "revision": "4af7dbbe9c04ccf8942b02254cdaba17",
+    "url": "/EnextTesteFrontEnd/static/media/bag-icon.4af7dbbe.svg"
+  },
+  {
+    "revision": "bf825c62e8a48b8bd6208e5a5ca62dcc",
+    "url": "/EnextTesteFrontEnd/index.html"
+  }
+];
